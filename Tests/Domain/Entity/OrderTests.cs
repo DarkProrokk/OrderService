27f3ec313dc5ef36499b5e6560ada39837c25177
DarkProrok <seroshtanmakcs@gmail.com>
2025-03-13 20:30:35 +1000

    Add Test project and tests for order in domain

---
+++ Tests/Domain/Entity/OrderTests.cs
@@ -0,0 +1,49 @@
+using Domain.Entity;
+using static System.Guid;
+
+namespace Tests.Domain.Entity;
+
+public class OrderTests
+{
+    [Fact]
+    public void Constructor_ShouldNotBeNull()
+    {
+        // Arrange
+        var userGuid = NewGuid();
+        var productId = NewGuid();
+        var productIds = new List<Guid> { productId };
+
+        //Act
+        var order = new Order(userGuid, productIds);
+        
+        //Assert
+        Assert.NotNull(order);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_WhenProductsListIsEmpty()
+    {
+        // Arrange
+        var userGuid = NewGuid();
+        var productIds = new List<Guid>();
+        
+        //Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => new Order(userGuid, productIds));
+        Assert.Equal("The number of products must be greater than 0.", exception.Message);
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnCorrectItemInfo()
+    {
+        // Arrange
+        var userGuid = NewGuid();
+        var productId = NewGuid();
+        var productIds = new List<Guid> { productId };
+
+        //Act
+        var order = new Order(userGuid, productIds);
+        
+        //Assert
+        Assert.Equal($"Id: {order.Id}, UserId: {userGuid}, Products: {string.Join(", ", productIds)}", order.ToString());
+    }
+}
